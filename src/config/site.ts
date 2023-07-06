aab303dcd2c1dee59d90341c0407053d0340dfdd
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-07-06 13:41:31 +0900

    FEAT: page config setting

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "형진의 blog",
+  description: "박형진 입니다.",
+  url: "https://hywlss.github.io",
+  ogImage: "https://hywlss.github.io/og.png",
+  links: {
+    github: "https://github.com/hywlss9",
+  },
+};

e9d57fdf257c45519a13c4513cd5bbedc30514ba
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-07-06 16:15:22 +0900

    FEAT: posts preview data 보여주기

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,11 +1,20 @@
+import { POSTS_PREVIEWS } from "@/constants/posts";
+
 export default function Posts() {
   return (
     <main>
       <h2>글 목록</h2>
       <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
+        {POSTS_PREVIEWS.map(({ id, title, description, date, tags }) => (
+          <li key={id} className="flex-col">
+            <strong>{title}</strong>
+            <p>{description}</p>
+            <span>{date}</span>
+            {tags.map((tag) => (
+              <span key={tag}>{tag}</span>
+            ))}
+          </li>
+        ))}
       </ul>
     </main>
   );

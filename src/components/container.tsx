a84a7c31632713b822167987c1ef9da4ae0251c2
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-07-06 19:13:09 +0900

    FIX: Container min-height 추가
    calc 적용

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -3,5 +3,5 @@ import type { HTMLAttributes } from "react";
 interface ContainerProps extends HTMLAttributes<HTMLDivElement> {}
 
 export default function Container({ children }: ContainerProps) {
-  return <main className="p-6">{children}</main>;
+  return <main className="min-h-[calc(100vh-3rem)] p-6">{children}</main>;
 }

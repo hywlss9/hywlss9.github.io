732ac3e8c7e025d9389822a1f64c23e734d471a0
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-07-09 04:39:32 +0900

    FIX: posts 글 순서 최신순으로 변경

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -6,11 +6,15 @@ import Container from "@/components/container";
 import Tag from "@/components/tag";
 
 export default function Posts() {
+  const list = POSTS_PREVIEWS.sort((a, b) =>
+    new Date(a.date) > new Date(b.date) ? -1 : 1
+  );
+
   return (
     <Container>
       <h2>글 목록</h2>
       <ul>
-        {POSTS_PREVIEWS.map(({ id, title, description, date, tags }) => (
+        {list.map(({ id, title, description, date, tags }) => (
           <li key={id} className="group flex-col border-b last:border-b-0">
             <Link href={{ pathname: `/posts/${id}` }} className="block py-4">
               <strong className="block mb-2 text-xl group-hover:underline">

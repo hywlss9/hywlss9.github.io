e06bad2636f5decdf4b52e12de8835489ee96ad7
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-07-10 03:42:32 +0900

    FEAT: markdown 블로그 글 추가
    - html로 파싱

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,30 @@
+import { marked } from "marked";
+
+import { siteConfig } from "@/config/site";
+
+import Container from "@/components/container";
+
+export default async function Post({
+  params,
+}: {
+  params: { [key: string]: string };
+}) {
+  const { id } = params;
+
+  try {
+    const res = await fetch(`${siteConfig.url}/posts/${id}/${id}.md`);
+    const data = await res.text();
+    const html = marked.parse(data);
+
+    return (
+      <Container>
+        <div
+          className="[&>p]:leading-8 [&>table]:border-collapse [&>table>thead>tr>th]:border [&>table>thead>tr>th]:p-3 [&>table>tbody>tr>td]:border [&>table>tbody>tr>td]:p-3"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </Container>
+    );
+  } catch {
+    return <>파일이 없습니다.</>;
+  }
+}

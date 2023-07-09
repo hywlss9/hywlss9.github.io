aacca109a60067e75dcf22127cfab67704ee24bf
HyeongJin Park <37681882+hywlss9@users.noreply.github.com>
2023-07-10 01:47:44 +0900

    FIX: site config url 오타 수정, 개발 환경 url 추가

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,8 +3,11 @@ import type { SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
   name: "형진의 blog",
   description: "박형진 입니다.",
-  url: "https://hywlss.github.io",
-  ogImage: "https://hywlss.github.io/og.png",
+  url:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://hywlss9.github.io",
+  ogImage: "https://hywlss9.github.io/og.png",
   links: {
     github: "https://github.com/hywlss9",
   },

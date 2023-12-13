56a994be8f5066bbce5d78d77b77e88a7d45b100
HyeongJin Park <hywlss9@kw.ac.kr>
2023-12-13 13:46:55 +0900

    docs: 글 title, desc 수정

--- src/constants/posts.ts
+++ src/constants/posts.ts
@@ -10,17 +10,17 @@ export const POSTS_PREVIEWS: PostsPreview[] = [
   },
   {
     id: 1,
-    title: "JS Custom Event로 Modal 관리하기",
+    title: "JS Event Listener로 Modal 관리하기",
     description:
-      "React에서 Custom Event를 사용하여 Modal을 관리하는 방법을 소개합니다",
+      "React에서 Event Listener를 사용하여 Modal을 관리하는 방법을 소개합니다",
     date: "2022.04.20",
     tags: ["Front-End", "JavaScript", "TypeScript", "React"],
   },
   {
     id: 2,
-    title: "swagger에서 typescript data model 동기화하기",
+    title: "Swagger와 함께 openapi-typescript 사용하기",
     description:
-      "openapi-typescript를 사용하여 Back-End/Front-End의 data model을 동기화하는 방법을 소개합니다",
+      "openapi-typescript를 사용하여 Back-End/Front-End의 interface를 동기화하는 방법을 소개합니다",
     date: "2023.03.17",
     tags: ["Front-End", "Back-End", "TypeScript"],
   },

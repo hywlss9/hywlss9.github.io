67d6251e56874b7d6840e9540eb5e5c96de70537
HyeongJin Park <hywlss9@gmail.com>
2024-11-18 14:16:18 +0900

    feat: ga script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 
 import { defaultMetadata } from '@/config/site';
 
@@ -14,6 +15,15 @@ export const metadata: Metadata = defaultMetadata;
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='ko'>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script async src='https://www.googletagmanager.com/gtag/js?id=G-NRKWXMDHJB' />
+      <Script id='google analytics'>
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-NRKWXMDHJB');`}
+      </Script>
       <body className='dark:bg-dark-bg'>
         <Header />
         {children}
